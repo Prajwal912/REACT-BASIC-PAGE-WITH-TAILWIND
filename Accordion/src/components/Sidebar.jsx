2b485f0de9feb88a51a514aa741780fc8c52b33f
prajwal <prajwalshah912@gmail.com>
2023-09-17 22:50:22 +0530

    sidebar added with css design

--- Accordion/src/components/Sidebar.jsx
+++ Accordion/src/components/Sidebar.jsx
@@ -1,12 +1,120 @@
-import  {motion} from "framer-motion";
+import { motion } from "framer-motion";
+import { FaBars, FaHome, FaLock, FaUser } from "react-icons/fa";
+import { MdMessage } from "react-icons/md";
+import { BiAnalyse, BiSearch, BiCog } from "react-icons/bi";
+import { AiFillHeart, AiTwotoneFileExclamation } from "react-icons/ai";
+import { BsCartCheck } from "react-icons/bs";
+import { Link, NavLink } from "react-router-dom";
 
-const Sidebar = ({children}) => {
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    icon: <FaHome />,
+  },
+  {
+    path: "/users",
+    name: "Users",
+    icon: <FaUser />,
+  },
+  {
+    path: "/messages",
+    name: "Message",
+    icon: <MdMessage />,
+  },
+  {
+    path: "/analytics",
+    name: "Analytics",
+    icon: <BiAnalyse />,
+  },
+  {
+    path: "/file-manager",
+    name: "File Manager",
+    icon: <AiTwotoneFileExclamation />,
+    // subRoutes: [
+    //   {
+    //     path: "/settings/profile",
+    //     name: "Profile ",
+    //     icon: <FaUser />,
+    //   },
+    //   {
+    //     path: "/settings/2fa",
+    //     name: "2FA",
+    //     icon: <FaLock />,
+    //   },
+    //   {
+    //     path: "/settings/billing",
+    //     name: "Billing",
+    //     icon: <FaMoneyBill />,
+    //   },
+    // ],
+  },
+  {
+    path: "/order",
+    name: "Order",
+    icon: <BsCartCheck />,
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    icon: <BiCog />,
+    exact: true,
+    // subRoutes: [
+    //   {
+    //     path: "/settings/profile",
+    //     name: "Profile ",
+    //     icon: <FaUser />,
+    //   },
+    //   {
+    //     path: "/settings/2fa",
+    //     name: "2FA",
+    //     icon: <FaLock />,
+    //   },
+    //   {
+    //     path: "/settings/billing",
+    //     name: "Billing",
+    //     icon: <FaMoneyBill />,
+    //   },
+    // ],
+  },
+  {
+    path: "/saved",
+    name: "Saved",
+    icon: <AiFillHeart />,
+  },
+];
+
+const Sidebar = ({ children }) => {
   return (
     <div className="main-container">
-      <motion.div animate={{width:"200px"}} className="sidebar"></motion.div>
+      <motion.div animate={{ width: "200px" }} className="sidebar">
+        <div className="top--section">
+          <h1 className="logo">SideBar</h1>
+          <div className="bars">
+             <FaBars />
+          </div>
+        </div>
+      
+      <div className="search">
+        <div className="search--icons">
+          <BiSearch />
+        </div>
+
+        <input placeholder="Search..." />
+      </div>
+
+        <section className="routes">
+          {routes.map((route) => (
+            <NavLink to={route.path} key={route.name} className="link">
+              <div className="icon">{route.icon}</div>
+              <div className="link_text">{route.name}</div>
+            </NavLink>
+          ))}
+        </section>
+      </motion.div>
       <main>{children}</main>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar 
+export default Sidebar;
